c67ef8f278ff29f8ba2b51f6649ecae1952de27f
zhaoyingzhuo <zhaoyingzhuo2@huawei.com>
2023-01-03 01:00:37 +0800

    read code 17

--- README.en.md
+++ README.en.md
@@ -11,13 +11,14 @@ Yet another STL for learning.
 ├── construct.h         对象的构造和析构                            100%
 ├── exceptdef.h         调试宏文件                                  100%
 ├── functional.h
-├── heap_algo.h
+├── heap_algo.h         堆算法操作，给priority_queue用               50%
 ├── iterator.h          迭代器的定义                                100%
 ├── memory.h            一些高级内存管理函数，似乎没用上               N/A
 ├── type_traits.h       偏特化头文件以及pair判断                     100%
-├── uninitialized.h     对未初始化的空间进行构造元素                 100%
+├── uninitialized.h     对未初始化的空间进行构造元素                  100%
 ├── util.h              给uninitialized.h使用的工具                  10%
 ├── list.h              list实现                                    100%
 ├── deque.h             deque实现                                   100%
-├── stack.h             stack adapter实现                             0%
+├── stack.h             stack adapter实现                           100%
+├── queue.h             queue和priority_queue adapter实现           100%
 └── vector.h            vector实现                                  100%
--- include/heap_algo.h
+++ include/heap_algo.h
@@ -2,51 +2,46 @@
 #define _INCLUDE_HEAP_ALGO_H_
 
 // 这个头文件包含 heap 的四个算法 : push_heap, pop_heap, sort_heap, make_heap
+// 本文件是以0作为根节点，左孩子和右孩子为2N + 1 和 2N + 2， 父亲节点为 (N - 1) / 2
 
 #include "iterator.h"
 
-namespace yastl
-{
+namespace yastl {
 
 /*****************************************************************************************/
 // push_heap
 // 该函数接受两个迭代器，表示一个 heap 容器的首尾，并且新元素已经插入到底部容器的最尾端，调整 heap
 /*****************************************************************************************/
 template <class RandomIter, class Distance, class T>
-void push_heap_aux(RandomIter first, Distance holeIndex, Distance topIndex, T value)
-{
+void push_heap_aux(RandomIter first, Distance holeIndex, Distance topIndex, T value) {
   auto parent = (holeIndex - 1) / 2;
-  while (holeIndex > topIndex && *(first + parent) < value)
-  {
+  while (holeIndex > topIndex && *(first + parent) < value) { // 如果没到根节点并且parent节点比value小，需要进行上滤（大顶堆）
     // 使用 operator<，所以 heap 为 max-heap
-    *(first + holeIndex) = *(first + parent);
-    holeIndex = parent;
+    *(first + holeIndex) = *(first + parent); // 先把hole的值填上父节点的值
+    holeIndex = parent; // 继续更新hole的位置
     parent = (holeIndex - 1) / 2;
   }
   *(first + holeIndex) = value;
 }
 
+// 在last是value的情况下进行上滤
 template <class RandomIter, class Distance>
-void push_heap_d(RandomIter first, RandomIter last, Distance*)
-{
+void push_heap_d(RandomIter first, RandomIter last, Distance*) {
   yastl::push_heap_aux(first, (last - first) - 1, static_cast<Distance>(0), *(last - 1));
 }
 
+// 在last是value的情况下进行上滤
 template <class RandomIter>
-void push_heap(RandomIter first, RandomIter last)
-{ // 新元素应该已置于底部容器的最尾端
+void push_heap(RandomIter first, RandomIter last) { // 新元素应该已置于底部容器的最尾端
   yastl::push_heap_d(first, last, distance_type(first));
 }
 
 // 重载版本使用函数对象 comp 代替比较操作
 template <class RandomIter, class Distance, class T, class Compared>
-void push_heap_aux(RandomIter first, Distance holeIndex, Distance topIndex, T value,
-                   Compared comp)
-{
+void push_heap_aux(RandomIter first, Distance holeIndex, Distance topIndex, T value, Compared comp) {
   auto parent = (holeIndex - 1) / 2;
-  while (holeIndex > topIndex && comp(*(first + parent), value))
-  {
-    *(first + holeIndex) = *(first + parent);
+  while (holeIndex > topIndex && comp(*(first + parent), value)) {
+    *(first + holeIndex) = *(first + parent); // 上滤
     holeIndex = parent;
     parent = (holeIndex - 1) / 2;
   }
@@ -54,15 +49,12 @@ void push_heap_aux(RandomIter first, Distance holeIndex, Distance topIndex, T va
 }
 
 template <class RandomIter, class Compared, class Distance>
-void push_heap_d(RandomIter first, RandomIter last, Distance*, Compared comp)
-{
-  yastl::push_heap_aux(first, (last - first) - 1, static_cast<Distance>(0),
-                       *(last - 1), comp);
+void push_heap_d(RandomIter first, RandomIter last, Distance*, Compared comp) {
+  yastl::push_heap_aux(first, (last - first) - 1, static_cast<Distance>(0), *(last - 1), comp);
 }
 
 template <class RandomIter, class Compared>
-void push_heap(RandomIter first, RandomIter last, Compared comp)
-{
+void push_heap(RandomIter first, RandomIter last, Compared comp) {
   yastl::push_heap_d(first, last, distance_type(first), comp);
 }
 
@@ -71,22 +63,20 @@ void push_heap(RandomIter first, RandomIter last, Compared comp)
 // 该函数接受两个迭代器，表示 heap 容器的首尾，将 heap 的根节点取出放到容器尾部，调整 heap
 /*****************************************************************************************/
 template <class RandomIter, class T, class Distance>
-void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value)
-{
+void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value) {
   // 先进行下溯(percolate down)过程
   auto topIndex = holeIndex;
   auto rchild = 2 * holeIndex + 2;
-  while (rchild < len)
-  {
-    if (*(first + rchild) < *(first + rchild - 1))
-      --rchild;
-    *(first + holeIndex) = *(first + rchild);
+  while (rchild < len) {
+    if (*(first + rchild) < *(first + rchild - 1)) { // 右孩子小于左孩子，将左孩子标记为更大的
+      --rchild; // 此处为左孩子的下标
+    }
+    *(first + holeIndex) = *(first + rchild); // 把较大的填过来，继续向下
     holeIndex = rchild;
     rchild = 2 * (rchild + 1);
   }
-  if (rchild == len)
-  {  // 如果没有右子节点
-    *(first + holeIndex) = *(first + (rchild - 1));
+  if (rchild == len) {  // 如果没有右子节点
+    *(first + holeIndex) = *(first + (rchild - 1)); // 就用左子节点填
     holeIndex = rchild - 1;
   }
   // 再执行一次上溯(percolate up)过程
@@ -94,31 +84,27 @@ void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value)
 }
 
 template <class RandomIter, class T, class Distance>
-void pop_heap_aux(RandomIter first, RandomIter last, RandomIter result, T value,
-                  Distance*)
-{
+void pop_heap_aux(RandomIter first, RandomIter last, RandomIter result, T value, Distance*) {
   // 先将首值调至尾节点，然后调整[first, last - 1)使之重新成为一个 max-heap
   *result = *first;
   yastl::adjust_heap(first, static_cast<Distance>(0), last - first, value);
 }
 
 template <class RandomIter>
-void pop_heap(RandomIter first, RandomIter last)
-{
+void pop_heap(RandomIter first, RandomIter last) {
   yastl::pop_heap_aux(first, last - 1, last - 1, *(last - 1), distance_type(first));
 }
 
-// 重载版本使用函数对象 comp 代替比较操作
+// 重载版本使用函数对象 comp 代替比较操作 关键字heapify，原地变堆
 template <class RandomIter, class T, class Distance, class Compared>
-void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value,
-                 Compared comp)
-{
+void adjust_heap(RandomIter first, Distance holeIndex, Distance len, T value, Compared comp) {
   // 先进行下溯(percolate down)过程
   auto topIndex = holeIndex;
   auto rchild = 2 * holeIndex + 2;
-  while (rchild < len)
-  {
-    if (comp(*(first + rchild), *(first + rchild - 1)))  --rchild;
+  while (rchild < len) {
+    if (comp(*(first + rchild), *(first + rchild - 1))) {
+      --rchild;
+    }
     *(first + holeIndex) = *(first + rchild);
     holeIndex = rchild;
     rchild = 2 * (rchild + 1);
@@ -198,27 +184,26 @@ void make_heap(RandomIter first, RandomIter last)
   yastl::make_heap_aux(first, last, distance_type(first));;
 }
 
-// 重载版本使用函数对象 comp 代替比较操作
+// 重载版本使用函数对象 comp 代替比较操作, 建立堆
 template <class RandomIter, class Distance, class Compared>
-void make_heap_aux(RandomIter first, RandomIter last, Distance*, Compared comp)
-{
-  if (last - first < 2)
+void make_heap_aux(RandomIter first, RandomIter last, Distance*, Compared comp) {
+  if (last - first < 2) {
     return;
+  }
   auto len = last - first;
   auto holeIndex = (len - 2) / 2;
-  while (true)
-  {
+  while (true) {
     // 重排以 holeIndex 为首的子树
     yastl::adjust_heap(first, holeIndex, len, *(first + holeIndex), comp);
-    if (holeIndex == 0)
+    if (holeIndex == 0) { // 到根节点
       return;
+    }
     holeIndex--;
   }
 }
 
 template <class RandomIter, class Compared>
-void make_heap(RandomIter first, RandomIter last, Compared comp)
-{
+void make_heap(RandomIter first, RandomIter last, Compared comp) {
   yastl::make_heap_aux(first, last, distance_type(first), comp);
 }
 
