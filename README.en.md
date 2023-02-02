fd9b72323b37d0779e061a12953cb8463c256213
zhaoyingzhuo <zhaoyingzhuo2@huawei.com>
2023-02-02 09:13:43 +0800

    read code 35: finish functor

--- README.en.md
+++ README.en.md
@@ -4,28 +4,28 @@
 Yet another STL for learning.
 
 #### 阅读进度
-.
-├── algobase.h          常用算法实现文件                            100%
-├── algo.h              算法实现文件                                100%
-├── allocator.h         分配器文件，预先定义的接口                   100%
-├── construct.h         对象的构造和析构                            100%
-├── exceptdef.h         调试宏文件                                  100%
-├── functional.h
-├── heap_algo.h         堆算法操作，给priority_queue用               100%
-├── iterator.h          迭代器的定义                                100%
-├── memory.h            一些高级内存管理函数，似乎没用上               N/A
-├── type_traits.h       偏特化头文件以及pair判断                     100%
-├── uninitialized.h     对未初始化的空间进行构造元素                  100%
-├── util.h              给uninitialized.h使用的工具                  10%
-├── list.h              list实现                                    100%
-├── deque.h             deque实现                                   100%
-├── stack.h             stack adapter实现                           100%
-├── queue.h             queue和priority_queue adapter实现           100%
-├── rb_tree.h           红黑树实现                                  100%
-├── map.h               map实现，依赖红黑树                          100%
-├── set.h               set实现，依赖红黑树                          100%
-├── set_algo.h          set算法实现，集合操作                        100%
-├── hashtable.h         哈希表实现                                  100%
-├── unordered_map.h     无序键值对集合操作，依赖哈希表                100%
-├── unordered_set.h     无需集合操作，依赖哈希表                      100%
-└── vector.h            vector实现                                  100%
+.  
+├── algobase.h          常用算法实现文件                            100%  
+├── algo.h              算法实现文件                                100%  
+├── allocator.h         分配器文件，预先定义的接口                   100%  
+├── construct.h         对象的构造和析构                            100%  
+├── exceptdef.h         调试宏文件                                  100%  
+├── functional.h        常用仿函数 functor                          100%  
+├── heap_algo.h         堆算法操作，给priority_queue用               100%  
+├── iterator.h          迭代器的定义                                100%  
+├── memory.h            一些高级内存管理函数，似乎没用上               N/A  
+├── type_traits.h       偏特化头文件以及pair判断                     100%  
+├── uninitialized.h     对未初始化的空间进行构造元素                  100%  
+├── util.h              给uninitialized.h使用的工具                  10%  
+├── list.h              list实现                                    100%  
+├── deque.h             deque实现                                   100%  
+├── stack.h             stack adapter实现                           100%  
+├── queue.h             queue和priority_queue adapter实现           100%  
+├── rb_tree.h           红黑树实现                                  100%  
+├── map.h               map实现，依赖红黑树                          100%  
+├── set.h               set实现，依赖红黑树                          100%  
+├── set_algo.h          set算法实现，集合操作                        100%  
+├── hashtable.h         哈希表实现                                  100%  
+├── unordered_map.h     无序键值对集合操作，依赖哈希表                100%  
+├── unordered_set.h     无需集合操作，依赖哈希表                      100%  
+└── vector.h            vector实现                                  100%  
--- include/functional.h
+++ include/functional.h
@@ -5,15 +5,13 @@
 
 #include <cstddef>
 
-namespace yastl
-{
+namespace yastl {
 
 // 定义一元函数的参数型别和返回值型别
 template <class Arg, class Result>
-struct unarg_function
-{
-  typedef Arg       argument_type;
-  typedef Result    result_type;
+struct unarg_function {
+  typedef Arg argument_type;
+  typedef Result result_type;
 };
 
 // 定义二元函数的参数型别的返回值型别
@@ -26,53 +24,63 @@ struct binary_function {
 
 // 函数对象：加法
 template <class T>
-struct plus :public binary_function<T, T, T>
-{
-  T operator()(const T& x, const T& y) const { return x + y; }
+struct plus :public binary_function<T, T, T> {
+  T operator()(const T& x, const T& y) const {
+    return x + y;
+  }
 };
 
 // 函数对象：减法
 template <class T>
-struct minus :public binary_function<T, T, T>
-{
-  T operator()(const T& x, const T& y) const { return x - y; }
+struct minus :public binary_function<T, T, T> {
+  T operator()(const T& x, const T& y) const {
+    return x - y;
+  }
 };
 
 // 函数对象：乘法
 template <class T>
-struct multiplies :public binary_function<T, T, T>
-{
-  T operator()(const T& x, const T& y) const { return x * y; }
+struct multiplies :public binary_function<T, T, T> {
+  T operator()(const T& x, const T& y) const {
+    return x * y;
+  }
 };
 
 // 函数对象：除法
 template <class T>
-struct divides :public binary_function<T, T, T>
-{
-  T operator()(const T& x, const T& y) const { return x / y; }
+struct divides :public binary_function<T, T, T> {
+  T operator()(const T& x, const T& y) const {
+    return x / y;
+  }
 };
 
 // 函数对象：模取
 template <class T>
-struct modulus :public binary_function<T, T, T>
-{
-  T operator()(const T& x, const T& y) const { return x % y; }
+struct modulus :public binary_function<T, T, T> {
+  T operator()(const T& x, const T& y) const {
+    return x % y;
+  }
 };
 
 // 函数对象：否定
 template <class T>
-struct negate :public unarg_function<T, T>
-{
-  T operator()(const T& x) const { return -x; }
+struct negate :public unarg_function<T, T> {
+  T operator()(const T& x) const {
+    return -x;
+  }
 };
 
 // 加法的证同元素
 template <class T>
-T identity_element(plus<T>) { return T(0); }
+T identity_element(plus<T>) {
+  return T(0);
+}
 
 // 乘法的证同元素
 template <class T>
-T identity_element(multiplies<T>) { return T(1); }
+T identity_element(multiplies<T>) {
+  return T(1);
+}
 
 // 函数对象：等于
 template <class T>
@@ -84,16 +92,18 @@ struct equal_to : public binary_function<T, T, bool> {
 
 // 函数对象：不等于
 template <class T>
-struct not_equal_to :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x != y; }
+struct not_equal_to :public binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const {
+    return x != y;
+  }
 };
 
 // 函数对象：大于
 template <class T>
-struct greater :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x > y; }
+struct greater :public binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const {
+    return x > y;
+  }
 };
 
 // 函数对象：小于
@@ -106,78 +116,82 @@ struct less : public binary_function<T, T, bool> {
 
 // 函数对象：大于等于
 template <class T>
-struct greater_equal :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x >= y; }
+struct greater_equal :public binary_function<T, T, bool>  {
+  bool operator()(const T& x, const T& y) const {
+    return x >= y;
+  }
 };
 
 // 函数对象：小于等于
 template <class T>
-struct less_equal :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x <= y; }
+struct less_equal :public binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const {
+    return x <= y;
+  }
 };
 
 // 函数对象：逻辑与
 template <class T>
-struct logical_and :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x && y; }
+struct logical_and :public binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const {
+    return x && y;
+  }
 };
 
 // 函数对象：逻辑或
 template <class T>
-struct logical_or :public binary_function<T, T, bool>
-{
-  bool operator()(const T& x, const T& y) const { return x || y; }
+struct logical_or :public binary_function<T, T, bool> {
+  bool operator()(const T& x, const T& y) const {
+    return x || y;
+  }
 };
 
 // 函数对象：逻辑非
 template <class T>
-struct logical_not :public unarg_function<T, bool>
-{
-  bool operator()(const T& x) const { return !x; }
+struct logical_not :public unarg_function<T, bool> {
+  bool operator()(const T& x) const {
+    return !x;
+  }
 };
 
 // 证同函数：不会改变元素，返回本身
 template <class T>
-struct identity :public unarg_function<T, bool>
-{
-  const T& operator()(const T& x) const { return x; }
+struct identity :public unarg_function<T, bool> {
+  const T& operator()(const T& x) const {
+    return x;
+  }
 };
 
 // 选择函数：接受一个 pair，返回第一个元素
 template <class Pair>
-struct selectfirst :public unarg_function<Pair, typename Pair::first_type>
-{
-  const typename Pair::first_type& operator()(const Pair& x) const
-  {
+struct selectfirst :public unarg_function<Pair, typename Pair::first_type> {
+  const typename Pair::first_type& operator()(const Pair& x) const {
     return x.first;
   }
 };
 
 // 选择函数：接受一个 pair，返回第二个元素
 template <class Pair>
-struct selectsecond :public unarg_function<Pair, typename Pair::second_type>
-{
-  const typename Pair::second_type& operator()(const Pair& x) const
-  {
+struct selectsecond :public unarg_function<Pair, typename Pair::second_type> {
+  const typename Pair::second_type& operator()(const Pair& x) const {
     return x.second;
   }
 };
 
 // 投射函数：返回第一参数
 template <class Arg1, class Arg2>
-struct projectfirst :public binary_function<Arg1, Arg2, Arg1>
-{
-  Arg1 operator()(const Arg1& x, const Arg2&) const { return x; }
+struct projectfirst :public binary_function<Arg1, Arg2, Arg1> {
+  Arg1 operator()(const Arg1& x, const Arg2&) const {
+    return x;
+  }
 };
 
 // 投射函数：返回第二参数
 template <class Arg1, class Arg2>
-struct projectsecond :public binary_function<Arg1, Arg2, Arg1>
-{
-  Arg2 operator()(const Arg1&, const Arg2& y) const { return y; }
+struct projectsecond :public binary_function<Arg1, Arg2, Arg1> {
+  Arg2 operator()(const Arg1&, const Arg2& y) const {
+    return y;
+  }
 };
 
 /*****************************************************************************************/
@@ -189,10 +203,10 @@ struct hash {};
 
 // 针对指针的偏特化版本
 template <class T>
-struct hash<T*>
-{
-  size_t operator()(T* p) const noexcept
-  { return reinterpret_cast<size_t>(p); }
+struct hash<T*> {
+  size_t operator()(T* p) const noexcept {
+    return reinterpret_cast<size_t>(p);
+  }
 };
 
 // 对于整型类型，只是返回原值
@@ -246,8 +260,7 @@ inline size_t bitwise_hash(const unsigned char* first, size_t count)
   const size_t fnv_prime = 16777619u;
 #endif
   size_t result = fnv_offset;
-  for (size_t i = 0; i < count; ++i)
-  {
+  for (size_t i = 0; i < count; ++i) {
     result ^= (size_t)first[i];
     result *= fnv_prime;
   }
@@ -255,28 +268,22 @@ inline size_t bitwise_hash(const unsigned char* first, size_t count)
 }
 
 template <>
-struct hash<float>
-{
-  size_t operator()(const float& val)
-  { 
+struct hash<float> {
+  size_t operator()(const float& val) { 
     return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(float));
   }
 };
 
 template <>
-struct hash<double>
-{
-  size_t operator()(const double& val)
-  {
+struct hash<double> {
+  size_t operator()(const double& val) {
     return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(double));
   }
 };
 
 template <>
-struct hash<long double>
-{
-  size_t operator()(const long double& val)
-  {
+struct hash<long double> {
+  size_t operator()(const long double& val) {
     return val == 0.0f ? 0 : bitwise_hash((const unsigned char*)&val, sizeof(long double));
   }
 };
